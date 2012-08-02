7c76e1a17907de09fb12ec6c99afcb29ff00768b
liwei <hi@liwei.me>
2012-08-02 20:55:14 +0800

    first

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'zggzbhcom';
+$cfg_dbuser = 'zggzbh';
+$cfg_dbpwd  = 'zggzbh@123';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
